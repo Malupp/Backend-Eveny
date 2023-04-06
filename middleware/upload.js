9e4d5ba517334d1a5f72edacbd68fdef233bed63
Manuel Luppino <60667104+Warlup@users.noreply.github.com>
2023-04-06 10:42:53 +0200

    add multer and uploads files

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import * as fs from 'fs'
+import path from 'path';
 
 const router = express.Router();
 
 const createUploadRoute = (upload) => {
   router.post('/', upload.single('file'), (req, res) => {
     res.json({ filename: req.file.filename });
+  });
+
+  router.get('/:fileName', (req, res) => {
+    const file = fs.readFileSync(`${path.join(path.dirname(import.meta.url), '..', 'uploads', req.params.fileName).replace('file:\\', '')}`)
+    res.send(file)
   });
 
   return router;
